d71fc90be8187f26ade0727709eb217ee05d9a9d
Brito <>
2021-04-25 16:27:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from roman_numeral import RomanNumerals
+
+print(RomanNumerals.to_roman(987))
+print(RomanNumerals.to_roman(1978))
+print(RomanNumerals.from_roman('CMLXXXVII'))
+print(RomanNumerals.from_roman('MCMLXXVIII'))
+
+print(dir(RomanNumerals))
+print(help(RomanNumerals))
---
+++ roman_numeral.py
@@ -0,0 +1,46 @@
+class RomanNumerals:
+    arabic = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
+
+    roman = {v: k for k, v in sorted(arabic.items())}
+    
+    @classmethod
+    def to_roman(self, num):
+        result = ''
+        for i in self.arabic:
+            while num >= i:
+                num -= i
+                result += self.arabic.get(i)
+        return result
+    
+    @classmethod
+    def from_roman(self, num):
+        result = 0
+        for i in range(len(num)):
+            if i == 0 or self.roman.get(num[i]) <= self.roman.get(num[i-1]):
+                result += self.roman.get(num[i])
+            else:
+                result += self.roman.get(num[i]) - 2 * self.roman.get(num[i - 1])
+        return result
+
+
+
+def transform_roman_numeral_to_number(roman_numeral):
+    roman_char_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    for i in range(0, len(roman_numeral)):
+        if i == 0 or roman_char_dict[roman_numeral[i]] <= roman_char_dict[roman_numeral[i - 1]]:
+            res += roman_char_dict[roman_numeral[i]]
+        else:
+            res += roman_char_dict[roman_numeral[i]] - 2 * roman_char_dict[roman_numeral[i - 1]]
+    return res
+
+
+def transform_number_to_roman_numeral(number):
+    roman_value_list = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    roman_char_list = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    res = ''
+    for i in range(len(roman_value_list)):
+        while number >= roman_value_list[i]:
+            number -= roman_value_list[i]
+            res += roman_char_list[i]
+    return res
